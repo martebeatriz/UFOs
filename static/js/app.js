73fb0a1b1c6b196c3390d65184dd2de02a5f3e54
Beatriz <owner1@MacBook-Air.local>
2022-08-26 21:09:07 -0400

    website completed

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,8 @@
-// // import the data from data.js
-// const tableData = data;
+// import the data from data.js
+const tableData = data;
 
-// // Reference the HTML table using d3
-// var tbody = d3.select("tbody");
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
 
 
 
